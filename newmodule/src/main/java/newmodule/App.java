3e5334933d29d2f05b15a7553fb52730a9a0e7af
wolender <wolender@griddynamics.com>
2023-03-31 16:01:33 +0200

    adding assembly pluggin

--- newmodule/src/main/java/newmodule/App.java
+++ newmodule/src/main/java/newmodule/App.java
@@ -4,10 +4,10 @@ package newmodule;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
 }
